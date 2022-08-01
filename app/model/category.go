c72f0189c5f88266205762ed9808068cbbbe4d9a
armadamuhammad <armada_muhammad@student.uns.ac.id>
2022-08-01 22:02:15 +0700

    category

--- app/model/category.go
+++ app/model/category.go
@@ -12,21 +12,22 @@ type Category struct {
 	Base
 	DataOwner
 	CategoryAPI
-	Balance *Balance `json:"balance,omitempty" gorm:"foreignKey:BalanceID;references:ID"`
+	Balance *Balance  `json:"balance,omitempty" gorm:"foreignKey:BalanceID;references:ID"`
+	Parent  *Category `json:"parent,omitempty" gorm:"foreignKey:ParentID;references:ID"`
 	// Child   *[]Category `json:"child,omitempty" gorm:"foreignKey:ID;references:ParentID"`
 }
 
 // CategoryAPI Category API
 type CategoryAPI struct {
-	Name        *string    `json:"name,omitempty" example:"infaq" gorm:"type:varchar(256)"`                         // Name
-	Code        *string    `json:"code,omitempty" example:"12" gorm:"type:varchar(256)"`                            // Code
-	Level       *int       `json:"level,omitempty" gorm:"type:smallint"`                                            // Level
-	Category    *string    `json:"category,omitempty" gorm:"type:varchar(256)"`                                     // Category
-	IsIncome    *int       `json:"is_income,omitempty" example:"1"`                                                 // 1 = income 0 = outcome
-	Anonym      *int       `json:"Anonym,omitempty" example:"1"`                                                    // 1 = can be anonym transaction 0 = should login
-	Description *string    `json:"description,omitempty" example:"lorem ipsum" gorm:"type:text"`                    // Description
-	ParentID    *uuid.UUID `json:"parent_id,omitempty" gorm:"type:varchar(36)" swaggertype:"string" format:"uuid"`  // ParentID
-	BalanceID   *uuid.UUID `json:"balance_id,omitempty" gorm:"type:varchar(36)" swaggertype:"string" format:"uuid"` // BalanceID
+	Name        *string    `json:"name,omitempty" example:"infaq" gorm:"type:varchar(256)"`                                                             // Name
+	Code        *string    `json:"code,omitempty" example:"12" gorm:"type:varchar(256);index:,unique,where:deleted_at is null"` // Code
+	Level       *int       `json:"level,omitempty" gorm:"type:smallint"`                                                                                // Level
+	Category    *string    `json:"category,omitempty" gorm:"type:varchar(256)"`                                                                         // Category
+	IsIncome    *int       `json:"is_income,omitempty" example:"1"`                                                                                     // 1 = income 0 = outcome
+	Anonym      *int       `json:"Anonym,omitempty" example:"1"`                                                                                        // 1 = can be anonym transaction 0 = should login
+	Description *string    `json:"description,omitempty" example:"lorem ipsum" gorm:"type:text"`                                                        // Description
+	ParentID    *uuid.UUID `json:"parent_id,omitempty" gorm:"type:varchar(36)" swaggertype:"string" format:"uuid"`                                      // ParentID
+	BalanceID   *uuid.UUID `json:"balance_id,omitempty" gorm:"type:varchar(36)" swaggertype:"string" format:"uuid"`                                     // BalanceID
 }
 
 func (s *Category) Seed() *[]Category {
